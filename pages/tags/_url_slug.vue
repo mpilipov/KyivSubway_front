3950b1ff29408b5aae931600afd554fcee85e99c
mpilipov <misha7538@live.ru>
2022-04-11 15:03:15 +0300

    Update _url_slug.vue

--- pages/tags/_url_slug.vue
+++ pages/tags/_url_slug.vue
@@ -30,7 +30,8 @@
                   <nuxt-link
                     :to="`/posts/${post.url_slug}`"
                     class="btn btn-sm btn-outline-secondary"
-                    >Подробнее</nuxt-link>
+                    >Подробнее</nuxt-link
+                  >
                 </div>
                 <small class="text-muted">{{ post.created }}</small>
               </div>
@@ -38,21 +39,86 @@
           </div>
         </div>
       </div>
+
+      <!-- Пагинация -->
+      <nav aria-label="Paginate me">
+        <ul class="pagination justify-content-center">
+          <nuxt-link
+            v-if="previous != null"
+            class="page-link"
+            :to="previous"
+            tabindex="-1"
+            >Предыдущая</nuxt-link
+          >
+          <li v-else class="page-item disabled">
+            <a class="page-link disabled" href="#" tabindex="-1">Предыдущая</a>
+          </li>
+
+          <span v-for="i in total">
+            <li
+              v-if="current_page === i || ($route.query.page === '/' && i === 1)"
+              class="page-item active"
+            >
+              <nuxt-link class="page-link" :to="`?page=${i}`">{{ i }}</nuxt-link>
+            </li>
+            <li v-else class="page-item">
+              <nuxt-link class="page-link" :to="`?page=${i}`">{{ i }}</nuxt-link>
+            </li>
+          </span>
+
+          <nuxt-link v-if="next != null" class="page-link" :to="next"
+            >Следующая</nuxt-link
+          >
+          <li v-else class="page-item disabled">
+            <a class="page-link" href="#">Следующая</a>
+          </li>
+        </ul>
+      </nav>
+
+
+
+
+
     </div>
+    <div v-if="posts.length <= 3">
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+    
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default {
-  components: { Header },
+  components: { Header, Footer },
   layout: "post_detail",
-  async asyncData({ params }) {
+  watchQuery: ['page'],
+  data() {
+    return {
+      posts: [],
+      tags: [],
+      total: [],
+      next: [],
+      title: [],
+      previous: [],
+      current_page: 0,
+    };
+  },
+
+  async asyncData({ params, route }) {
     // Сначала получаем весь список тегов
-    const tags = await axios.get(`http://127.0.0.1:8000/api/tags/`);
+console.log('1');
+
+    const tags = await axios.get(`http://51.250.71.220/api/tags/`);
+console.log('2');
+
     var t;
-    console.log(tags.data.length);
+    console.log(tags);
     // Затем определяем, какому Tags.name соответствует params.url_slug
     // Необходимо, чтобы Сериалайзер тегов в бекенде возвращал fields = ("name", "slug", )
     for (var i = 0; i < tags.data.length; i += 1) {
@@ -60,9 +126,18 @@ export default {
         t = tags.data[i].slug;
       }
     }
-    const { data } = await axios.get(`http://127.0.0.1:8000/api/tags/${t}/`);
+    let mypage = route.query.page !== undefined ? `?page=${route.query.page}` : ``;
+    console.log(mypage);
+    const { data } = await axios.get(`http://127.0.0.1:8000/api/tags/${t}/${mypage}`);
+    let next = data.next != null ? data.next.split("/")[5] : data.next;
+    let previous = data.previous != null ? data.previous.split("/")[5] : data.previous;
+    let current_page = route.query.page;
     return {
       posts: data.results,
+      total: Math.ceil(data.count / 6),
+      next: next,
+      previous: previous,
+      current_page: Number(current_page),
       title: `#${params.url_slug}`,
       tags: tags.data,
     };
